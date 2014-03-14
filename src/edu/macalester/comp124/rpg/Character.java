4f8d75fd5dbbb931f3d49a70e49037ff85d4c288
lbarstow <lbarstow@macalester.edu>
2014-03-13 20:04:14 -0500

    Basic hardcoded map with a wall. Character can't walk through wall, but there is a runtime error that crashes if you try to walk out of bounds.

---
+++ src/edu/macalester/comp124/rpg/Character.java
@@ -0,0 +1,14 @@
+package edu.macalester.comp124.rpg;
+
+/**
+ * Created by Laura on 3/13/14.
+ */
+public class Character {
+    String name;
+
+
+    public Character(String name){
+        this.name = name;
+    }
+
+}
--- src/edu/macalester/comp124/rpg/Map.java
+++ src/edu/macalester/comp124/rpg/Map.java
@@ -4,54 +4,62 @@ package edu.macalester.comp124.rpg;
  * Created by Laura on 3/13/14.
  */
 public class Map {
+    private static int WIDTH = 10;
+    private static int HEIGHT = 10;
+    private static int WALL_LEFT_BOUND = 3;
+    private static int WALL_RIGHT_BOUND = 6;
+    private static int WALL_TOP = 2;
+    private static int WALL_BOTTOM = 7;
+
     char pChar = '@';
     char filler = '.';
-
-    char tChar;
-    int height, width;
     int pX, pY;
-    int tX, tY;
     char[][] map;
 
 
-    public Map(int width, int height, char tC){
-        this.height= height;
-        this.width = width;
-        this.map= new char[width][height];
-        this.tChar = tC;
-        fillMatrix(filler);
 
+    public Map(){
+        map= new char[WIDTH][HEIGHT];
+        pX = 0;
+        pY = 0;
+        fillMatrix();
     }
 
-    public void fillMatrix(char r){
-        for (int x = 0; x<width; x++){
-            for (int y = 0; y<height; y++){
-                map[x][y]=r;
+    public void fillMatrix(){
+        for (int x = 0; x<WIDTH; x++){
+            for (int y = 0; y<HEIGHT; y++){
+                if (y==WALL_TOP && x>=WALL_LEFT_BOUND && x<= WALL_RIGHT_BOUND) {
+                   map[x][y]= 'w';
+                } else if (x==WALL_LEFT_BOUND && y>=WALL_TOP && y<= WALL_BOTTOM){
+                   map[x][y] = 'w';
+                } else {
+                   map[x][y] = '.';
+                }
             }
         }
+        map[pX][pY] = pChar;
     }
 
     public void setPlayer(int x, int y ){
-        pX=x;
-        pY=y;
-        if (x<0){
-            pX=0;
-        } else if(x>width-1){
-            pX=width-1;
+        int oldX = pX;
+        int oldY = pY;
+        if (map[x][y]!= 'w'){
+            pX=x;
+            pY=y;
+            if (x<0){
+                pX=0;
+            } else if(x>=WIDTH){
+                pX=WIDTH-1;
+            } else if(y<0){
+                pY=0;
+            } else if (y>=HEIGHT){
+                pY=HEIGHT-1;
+            }
+            map[oldX][oldY] = filler;
         }
-        if(y<0){
-            pY=0;
-        } else if (y>height-1){
-            pY=height-1;
-        }
-        map[pX][pY]=pChar;
+        this.map[pX][pY]=pChar;
     }
 
-    public void setTarget(int x, int y){
-        tX=x;
-        tY=y;
-        map[tX][tY]=tChar;
-    }
 
     public void movePlayer(char c){
         map[pX][pY] = filler;
@@ -72,8 +80,8 @@ public class Map {
     @Override
     public String toString(){
         String content = "";
-        for (int y = 0; y<height; y++){
-            for (int x =0; x<width; x++){
+        for (int y = 0; y<HEIGHT; y++){
+            for (int x =0; x<WIDTH; x++){
                 content+= map[x][y];
             }
             content += System.lineSeparator();
