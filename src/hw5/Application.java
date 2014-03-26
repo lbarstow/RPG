acc8bf1e20eaad69ef89120385f2d72ed5fa597d
lbarstow <lbarstow@macalester.edu>
2014-03-26 14:22:40 -0500

    Merge branch 'master' of https://github.com/hjfender/RPG
    Conflicts:
    	maps/impassible terrain.txt
    	src/hw5/CombatForm.java
    	src/hw5/Player.java

--- src/hw5/Application.java
+++ src/hw5/Application.java
@@ -12,7 +12,15 @@ public class Application
 		//--- Formally, it's called the Model
 		//--- The thing that draws the picture is called the View
 		//--- The thing that lets players select actions is the Controller
-		Game theGame = new Game();
+        //CombatForm woo = new CombatForm();
+        //woo.setVisible(true);
+        //CombatForm woo = new CombatForm();
+        //woo.setVisible(true);
+
+        CombatForm combo = new CombatForm();
+        combo.setVisible(true);
+
+		//Game theGame = new Game();
 
 
 		// TODO: Load character screen, create/edit character
@@ -20,7 +28,7 @@ public class Application
 
 		//--- The map screen is a View of our game
 		//--- It's also our Controller when navigating the map
-		MainForm mapScreen = new MainForm(theGame);
-		mapScreen.setVisible(true);
+		//MainForm mapScreen = new MainForm(theGame);
+		//mapScreen.setVisible(true);
 	}
 }
